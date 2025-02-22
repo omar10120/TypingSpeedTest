721aee133ba90037c07704437da16c5037861f9f
tamer tamer <amr.dawoodi@hotmail.com>
2025-02-22 12:16:28 +0300

    fix commonWords

--- src/components/TypingTest.tsx
+++ src/components/TypingTest.tsx
@@ -36,7 +36,7 @@ export default function TypingTest({ duration = 60 }: TypingTestProps) {
       words.push(commonWords[randomIndex]);
     }
     return words;
-  }, []);
+  }, [commonWords]);
 
   useEffect(() => {
     setWordList(generateWords());
